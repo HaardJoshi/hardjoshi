df2c5aebbf9c1d38d5f8d78bf7057bb0405965a9
Hard Joshi <hardjoshi.dev@gmail.com>
2025-11-03 16:43:19 +0000

    resolved error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import sourceIdentifierPlugin from "vite-plugin-source-identifier";
 
-const isProd = process.env.BUILD_MODE === 'prod'
+const isProd = process.env.BUILD_MODE === "prod";
 export default defineConfig({
-  base: "/hardjoshi/"
+  base: "/hardjoshi/",
   plugins: [
-    react(), 
+    react(),
     sourceIdentifierPlugin({
       enabled: !isProd,
-      attributePrefix: 'data-matrix',
+      attributePrefix: "data-matrix",
       includeProps: true,
-    })
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
